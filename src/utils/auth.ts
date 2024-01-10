69ffb30eae4d18445144417ce6f98fddf2ba2c8e
Le Duc Vinh <vinhle@Les-MacBook-Pro.local>
2024-01-10 17:16:15 +0700

    Add refresh token functionality to handle expired access tokens

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,17 +2,27 @@ import { User } from '@/types/user.type'
 
 export const LocalStorageEventTarget = new EventTarget()
 
-export const setAuthToLS = (token: string, user: User) => {
-  localStorage.setItem('access_token', token)
-  localStorage.setItem('profile', JSON.stringify(user))
+export const setAuthToLS = (token?: string, user?: User) => {
+  token && localStorage.setItem('access_token', token)
+  user && localStorage.setItem('profile', JSON.stringify(user))
+}
+
+export const setRefreshTokenToLS = (token: string) => {
+  localStorage.setItem('refresh_token', token)
 }
 
 export const clearAuthFromLS = () => {
   localStorage.removeItem('access_token')
+  localStorage.removeItem('refresh_token')
   localStorage.removeItem('profile')
 
   const clearLSEvent = new Event('clearLS')
   LocalStorageEventTarget.dispatchEvent(clearLSEvent)
+}
+
+export const getRefreshTokenFromLS = () => {
+  const refreshToken = localStorage.getItem('refresh_token') || ''
+  return refreshToken
 }
 
 export const getAuthFromLS = () => {
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,22 +1,30 @@
 import HttpStatusCode from '@/constants/httpStatusCode.enum'
-import routerName from '@/router/routerName'
 import axios, { AxiosError, type AxiosInstance } from 'axios'
 import { toast } from 'react-toastify'
-import { clearAuthFromLS, getAuthFromLS, setAuthToLS } from './auth'
-import { AuthResponse } from '@/types/auth.type'
+import { clearAuthFromLS, getAuthFromLS, getRefreshTokenFromLS, setAuthToLS, setRefreshTokenToLS } from './auth'
+import { AuthResponse, RefreshTokenResponse } from '@/types/auth.type'
 import config from '@/constants/config'
+import { URL_LOGIN, URL_LOGOUT, URL_REFRESH_TOKEN, URL_REGISTER } from '@/apis/auth.api'
+import { isAxiosExpiredTokenError, isAxiosUnauthorizedError } from './utils'
+import { ErrorResponse } from '@/types/utils.type'
 
 class Http {
   instance: AxiosInstance
   private accessToken: string
+  private refreshToken: string
+  private refreshTokenRequest: Promise<string> | null
 
   constructor() {
     this.accessToken = getAuthFromLS().token
+    this.refreshToken = getRefreshTokenFromLS()
+    this.refreshTokenRequest = null
     ;(this.instance = axios.create({
       baseURL: config.baseUrl,
       timeout: 10000,
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'expire-access-token': 10,
+        'expire-refresh-token': 60 * 60
       }
     })),
       this.instance.interceptors.request.use(
@@ -35,17 +43,22 @@ class Http {
         (response) => {
           const url = response.config.url
 
-          if (url === routerName.login || url === routerName.register) {
+          if (url === URL_LOGIN || url === URL_REGISTER) {
             const data = response.data as AuthResponse
 
             this.accessToken = data.data.access_token
-            setAuthToLS(data.data.access_token, data.data.user)
+            this.refreshToken = data.data.refresh_token
 
-            toast.success(url === routerName.login ? 'Đăng nhập thành công' : 'Đăng ký và đăng nhập thành công', {
+            setAuthToLS(this.accessToken, data.data.user)
+            setRefreshTokenToLS(this.refreshToken)
+
+            toast.success(url === URL_LOGIN ? 'Đăng nhập thành công' : 'Đăng ký và đăng nhập thành công', {
               autoClose: 1000
             })
-          } else if (url === routerName.logout) {
+          } else if (url === URL_LOGOUT) {
             this.accessToken = ''
+            this.refreshToken = ''
+            clearAuthFromLS()
 
             toast.success('Đăng xuất thành công', {
               autoClose: 1000
@@ -54,21 +67,76 @@ class Http {
 
           return response
         },
-        function (error: AxiosError) {
-          if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+        (error: AxiosError) => {
+          // toast khi loi khong phai loi 422 va loi 401
+          if (
+            ![HttpStatusCode.UnprocessableEntity, HttpStatusCode.Unauthorized].includes(
+              error.response?.status as number
+            )
+          ) {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             const data: any | undefined = error.response?.data
             const message = data?.message || error.message
             toast.error(message)
           }
 
-          if (error.response?.status === HttpStatusCode.Unauthorized) {
+          // loi 401 thi có nhiều trường hợp
+          // 1. token hết hạn
+          // 2. token sai
+          // 3. không gửi token
+          if (isAxiosUnauthorizedError<ErrorResponse<{ name: string; message: string }>>(error)) {
+            const config = error?.response?.config
+            if (!config) return Promise.reject(error)
+
+            const { url } = config
+
+            // token het han va request refresh token chua duoc tao
+            if (isAxiosExpiredTokenError(error) && url !== URL_REFRESH_TOKEN) {
+              // han che goi token 2 lan
+              this.refreshTokenRequest = this.refreshTokenRequest
+                ? this.refreshTokenRequest
+                : this.handleRefreshToken().finally(() => {
+                    this.refreshTokenRequest = null
+                  })
+
+              return this.refreshTokenRequest?.then((access_token) => {
+                config.headers['Authorization'] = access_token
+
+                return this.instance(config)
+              })
+            }
+
+            // cac truong hop con lai
             clearAuthFromLS()
+            this.accessToken = ''
+            this.refreshToken = ''
+            toast.error(error.response?.data?.data?.message || error.response?.data.message)
           }
 
           return Promise.reject(error)
         }
       )
+  }
+
+  private handleRefreshToken() {
+    return this.instance
+      .post<RefreshTokenResponse>(URL_REFRESH_TOKEN, {
+        refresh_token: this.refreshToken
+      })
+      .then((response) => {
+        const { access_token } = response.data.data
+        this.accessToken = access_token
+        setAuthToLS(this.accessToken)
+
+        return access_token
+      })
+      .catch((error) => {
+        clearAuthFromLS()
+        this.accessToken = ''
+        this.refreshToken = ''
+
+        throw error
+      })
   }
 }
 
